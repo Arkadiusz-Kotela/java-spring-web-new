a54005e70a9b412f67d2c5a5dfa9619c2c69115b
Arkadiusz-Kotela <arkadiusz.kotela@gmail.com>
2018-11-14 17:09:54 +0300

    bb

--- src/main/java/com/crud/tasks/service/SimpleEmailService.java
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -7,9 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Service
 public class SimpleEmailService {
@@ -17,16 +17,18 @@ public class SimpleEmailService {
     private static final Logger LOGGER = LoggerFactory.getLogger(SimpleMailMessage.class);
 
     private final JavaMailSender javaMailSender;
+    private final MailCreatorService mailCreatorService;
 
     @Autowired
-    public SimpleEmailService(JavaMailSender javaMailSender) {
+    public SimpleEmailService(JavaMailSender javaMailSender, MailCreatorService mailCreatorService) {
         this.javaMailSender = javaMailSender;
+        this.mailCreatorService = mailCreatorService;
     }
 
     public void send(final Mail mail) {
         LOGGER.info("Starting email preparation");
         try {
-            javaMailSender.send(createMailMessage(mail));
+            javaMailSender.send(createMimeMessage(mail));
             LOGGER.info("Email has been fired out...");
         } catch (MailException e) {
             LOGGER.info("Failed to process email ...", e.getMessage(), e);
@@ -42,6 +44,18 @@ public class SimpleEmailService {
         mailMessage.setSubject(mail.getSubject());
         mailMessage.setText(mail.getMessage());
         return mailMessage;
+    }
+
+    private MimeMessagePreparator createMimeMessage(final Mail mail) {
+        return mimeMessage -> {
+            MimeMessageHelper mimeHelper = new MimeMessageHelper(mimeMessage);
+            mimeHelper.setTo(mail.getMailTo());
+            if (mail.getToCc() != null) {
+                mimeHelper.setCc(mail.getToCc());
+            }
+            mimeHelper.setSubject(mail.getSubject());
+            mimeHelper.setText(mailCreatorService.buildTrelloCardEmail(mail.getMessage()));
+        };
     }
 
 }
